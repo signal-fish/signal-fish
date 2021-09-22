658467ba2e228e1b253046cd2cc72a4b0b7cf1a6
Signal Fish <signalfish38861721@gmail.com>
2021-09-21 21:11:49 -0400

    change github pages link to gitee pages link

--- src/data.js
+++ src/data.js
@@ -36,7 +36,7 @@ const portfolios = [
     title: "animated moon",
     category: "css animation",
     image: "assets/portfolio/css-animation/animated-moon.png",
-    webLink: "https://signal-fish.github.io/animated-moon/",
+    webLink: "https://signal-fish.gitee.io/animated-moon/",
     sourceLink: "https://github.com/signal-fish/animated-moon",
   },
   {
@@ -44,7 +44,7 @@ const portfolios = [
     title: "animated tea cup",
     category: "css animation",
     image: "assets/portfolio/css-animation/animated-tea-cup.png",
-    webLink: "https://signal-fish.github.io/animated-tea-cup/",
+    webLink: "https://signal-fish.gitee.io/animated-tea-cup/",
     sourceLink: "https://github.com/signal-fish/animated-tea-cup",
   },
 ];
